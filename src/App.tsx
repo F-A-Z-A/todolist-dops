34cfc995106637f6229ebb6038d3810ff243afb5
AndreyUrazaev <f.a.z.a@bk.ru>
2024-10-09 16:32:00 +0500

    lesson-05

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,112 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Country } from "./Country";
+import { Todolist } from "./Todolist";
 import { v1 } from "uuid";
 
-export type BanknotesType = "USD" | "RUB" | "All";
-export type MoneyType = {
-  banknote: BanknotesType;
-  nominal: number;
+export type FilterValuesType = "all" | "active" | "completed";
+type TaskType = {
   id: string;
+  title: string;
+  isDone: boolean;
 };
-
-const defaultMoney: MoneyType[] = [
-  { banknote: "USD", nominal: 100, id: v1() },
-  { banknote: "USD", nominal: 100, id: v1() },
-  { banknote: "RUB", nominal: 100, id: v1() },
-  { banknote: "USD", nominal: 100, id: v1() },
-  { banknote: "USD", nominal: 100, id: v1() },
-  { banknote: "RUB", nominal: 100, id: v1() },
-  { banknote: "USD", nominal: 100, id: v1() },
-  { banknote: "RUB", nominal: 100, id: v1() },
-];
-
-export const moneyFilter = (money: MoneyType[], filter: BanknotesType): MoneyType[] => {
-  if (filter === "All") {
-    return money;
-  }
-  return money.filter((el) => el.banknote === filter);
+type TodolistsType = { id: string; title: string };
+type TasksStateType = {
+  [key: string]: InTaskType;
+};
+export type InTaskType = {
+  data: TaskType[];
+  filter: FilterValuesType;
 };
 
 function App() {
-  const [money, setMoney] = useState<MoneyType[]>(defaultMoney);
-  const [filterValue, setFilterValue] = useState<BanknotesType>("All");
-  const filteredMoney = moneyFilter(money, filterValue);
-  const addMoney = (banknote: BanknotesType) => {
-    setMoney([{ banknote: banknote, nominal: 100, id: v1() }, ...money]);
+  const todolistId1 = v1();
+  const todolistId2 = v1();
+
+  const [todolists, setTodolists] = useState<TodolistsType[]>([
+    { id: todolistId1, title: "What to learn" },
+    { id: todolistId2, title: "What to buy" },
+  ]);
+
+  const [tasks, setTasks] = useState<TasksStateType>({
+    [todolistId1]: {
+      data: [
+        { id: v1(), title: "HTML&CSS-1", isDone: true },
+        { id: v1(), title: "JS-1", isDone: true },
+      ],
+      filter: "all",
+    },
+    [todolistId2]: {
+      data: [
+        { id: v1(), title: "HTML&CSS-2", isDone: true },
+        { id: v1(), title: "JS-2", isDone: true },
+      ],
+      filter: "all",
+    },
+  });
+
+  const removeTodolist = (todolistId: string) => {
+    setTodolists(todolists.filter((el) => el.id !== todolistId));
+    delete tasks[todolistId];
   };
 
-  const removeMoney = (banknote: BanknotesType) => {
-    // const test = money.find((el) => el.banknote === banknote);
-    // if (test) {
-    //   setMoney(money.filter((el) => el.id !== test.id));
-    // }
-    const index = money.findIndex((el) => el.banknote === banknote);
-    if (index !== -1) {
-      setMoney(money.filter((_, i) => i !== index));
-    }
-  };
+  function removeTask(todolistId: string, taskId: string) {
+    setTasks({
+      ...tasks,
+      [todolistId]: {
+        ...tasks[todolistId],
+        data: tasks[todolistId].data.filter((el) => el.id !== taskId),
+      },
+    });
+  }
+
+  function addTask(todolistId: string, title: string) {
+    const newTask = { id: v1(), title: title, isDone: false };
+    setTasks({
+      ...tasks,
+      [todolistId]: {
+        ...tasks[todolistId],
+        data: [newTask, ...tasks[todolistId].data],
+      },
+    });
+  }
+
+  function changeStatus(todolistId: string, taskId: string, newIsDone: boolean) {
+    setTasks({
+      ...tasks,
+      [todolistId]: {
+        ...tasks[todolistId],
+        data: tasks[todolistId].data.map((t) => (t.id === taskId ? { ...t, isDone: newIsDone } : t)),
+      },
+    });
+  }
+
+  function changeFilter(todolistId: string, filter: FilterValuesType) {
+    setTasks({
+      ...tasks,
+      [todolistId]: { ...tasks[todolistId], filter },
+    });
+  }
 
   return (
     <div className="App">
-      <Country data={filteredMoney} setFilterValue={setFilterValue} addMoney={addMoney} removeMoney={removeMoney} />
+      {todolists.map((el) => {
+        return (
+          <Todolist
+            key={el.id}
+            todolistId={el.id}
+            title={el.title}
+            tasks={tasks[el.id].data}
+            removeTask={removeTask}
+            changeFilter={changeFilter}
+            addTask={addTask}
+            changeTaskStatus={changeStatus}
+            filter={tasks[el.id].filter}
+            removeTodolist={removeTodolist}
+          />
+        );
+      })}
     </div>
   );
 }
 
 export default App;
-
-// -------------------------------------------------------------------------------------------
-// import React, {useState} from 'react';
-// import './App.css';
-// import {Country} from "./Country";
-// import {v1} from "uuid";
-//
-// export type BanknotsType = '' // создадим типы для banknotes -он может быть 'DOLLARS', 'RUBLS' или 'All'
-// export type MoneyType = {
-//     banknote: BanknotsType
-//     nominal: any// не ленимся, убираем заглушку, и пишем правильный тип)
-//     id: any// ложку за Димыча, за...
-// }
-//
-// let defaultMoney: any = [  // типизируем
-//     {banknote: 'USD', nominal: 100, id: v1()},
-//     {banknote: 'USD', nominal: 100, id: v1()},
-//     {banknote: 'RUB', nominal: 100, id: v1()},
-//     {banknote: 'USD', nominal: 100, id: v1()},
-//     {banknote: 'USD', nominal: 100, id: v1()},
-//     {banknote: 'RUB', nominal: 100, id: v1()},
-//     {banknote: 'USD', nominal: 100, id: v1()},
-//     {banknote: 'RUB', nominal: 100, id: v1()},
-// ]
-//
-//
-// export const moneyFilter = (money: any, filter: any): any => {
-//     //если пришел filter со значением 'All', то возвращаем все банкноты
-//     //return money.filter... ну да, придется фильтровать
-// }
-//
-//
-// function App() {
-//     // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
-//     const [money, setMoney] = useState<any>([])
-//     const [filterValue, setFilterValue] = useState<any>('')   // по умолчанию указываем все банкноты
-//
-//     // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
-//     // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
-//     const filteredMoney = moneyFilter(грошы, фильтръ)
-//
-//     const addMoney = (banknote: BanknotsType) => {
-//         // Добавление денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
-//     }
-//
-//     const removeMoney = (banknote: BanknotsType) => {
-//         // Снятие денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
-//        // const index = money.findIndex
-//        //  if (index !== -1) {
-//        //      setMoney(money.filter((el, i) => ...));
-//        //  }
-//     }
-//
-//     return (
-//         <div className="App">
-//             <Country
-//                 data={filteredMoney}   //отрисовать будем деньги после фильтрации
-//                 setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
-//             />
-//         </div>
-//     );
-// }
-//
-// export default App;
